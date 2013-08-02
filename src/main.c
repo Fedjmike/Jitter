1d772ec9c984723bd5f8f9a5ca6516401816f711
Fedjmike <fedjmike@gmail.com>
2013-08-02 20:26:48 +0100

    Simple test case

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "ir.h"
+
+int main (int argc, char** argv) {
+    (void) argc, (void) argv;
+
+    /* 5*(4+9) */
+    ir_block* code = ir_create_block();
+    ir_block_finish(code, ir_create_return(
+            ir_block_add(code, ir_create_bop(ir_bop_mul,
+                               ir_block_add(code, ir_create_immediate(5)),
+                               ir_block_add(code, ir_create_bop(ir_bop_add,
+                                                  ir_block_add(code, ir_create_immediate(4)),
+                                                  ir_block_add(code, ir_create_immediate(9))
+                               ))
+            ))
+    ));
+
+    /*Run the code and print the result*/
+    printf(">> 5*(4+9)\n%d\n", runner(code));
+
+    ir_destroy_block(code);
+
+    return 0;
+}
